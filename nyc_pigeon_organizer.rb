66f41da4ae0dc91ab99ce72e1c4e9c985fb56815
Charlie Payne <charliepayne1101@gmail.com>
2020-04-18 16:05:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,29 +3,15 @@ def nyc_pigeon_organizer(original_data)
  original_data.each do |attribute, sub_attribute|
    sub_attribute.each do |sub_attribute, name|
      name.each do |name|
-       new_data[name] = {}
+       if !new_data[name]
+         new_data[name] = {}
      end
-     new_data[name][attribute]
+     if !new_data[name][attribute]
+     new_data[name][attribute] = []
    end
    new_data[name][attribute] << sub_attribute.to_s
  end
+ end
+ end
  new_data
  end
- 
-def nyc_pigeon_organizer(data)
-  new_hash = {}
-  data.each do |key, value|
-    value.each do |new_value, names|
-      names.each do |name|
-        if !new_hash[name]
-          new_hash[name] = {}
-        end
-        if !new_hash[name][key]
-          new_hash[name][key] = []
-        end
-        new_hash[name][key] << new_value.to_s
-      end
-    end
-  end
-  new_hash
-end
